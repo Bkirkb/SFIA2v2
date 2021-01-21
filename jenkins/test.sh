fcf6854d0028112d9c349d415efb733993002fd6
Bkirkb <b.kirkby489@gmail.com>
2021-01-21 20:10:47 +0000

    tweaking

--- jenkins/test.sh
+++ jenkins/test.sh
@@ -6,5 +6,5 @@ python3 -m venv venv
 source venv/bin/activate
 pip3 install -r jenkins/requirements.txt
 pip3 install flask_testing pytest pytest-cov
-python3 -m pytest --cov=service1 --cov=service2 --cov=service3 --cov=service4 --cov-report xml --cov-report term-missing --junitxml junit.xml
+python3 -m pytest --cov=service1 app --cov=service2 --cov=service3 --cov=service4 --cov-report xml --cov-report term-missing --junitxml junit.xml
 deactivate
