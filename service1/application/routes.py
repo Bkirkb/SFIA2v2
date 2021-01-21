e2e347859bd315496030978d7093563ca86869fc
Bkirkb <b.kirkby489@gmail.com>
2021-01-21 16:55:45 +0000

    feature2

--- service1/application/routes.py
+++ service1/application/routes.py
@@ -1,5 +1,5 @@
 from application import app, db
-from application.models import Fortune2
+from application.models import Fortune
 from flask import render_template
 import requests
 from sqlalchemy import desc
@@ -8,17 +8,17 @@ from sqlalchemy import desc
 @app.route('/index')
 def index():
     #target is name of container and port to be published
-    year_response = requests.get("http://fortune_app2-fortune-service2:5000/getyearmon")
-    luck_response = requests.get("http://fortune_app2-fortune-service3:5000/getluck")
-    fortune_response_post = requests.post("http://fortune_app2-fortune-service4:5000/getfortune", json={"year" : year_response.json()["year"] , "luck" : luck_response.text})
+    day_response = requests.get("http://fortune_app-fortune-service2:5000/getday")
+    luck_response = requests.get("http://fortune_app-fortune-service3:5000/getluck")
+    fortune_response_post = requests.post("http://fortune_app-fortune-service4:5000/getfortune", json={"day" : day_response.json()["day"] , "luck" : int(luck_response.text)})
     
-    luck = str(luck_response.text)
     fortunemsg = fortune_response_post.text
-    month = str(year_response.json()["month"])
-    year = str(year_response.json()["year"])
-    specdate = str(month + " " + year)
-    new_fortune = Fortune2(yearmon=specdate, fortune=fortunemsg, luck=luck)
+
+    day = str(day_response.json()["day"])
+    month = str(day_response.json()["month"])
+    specdate = str(day + " " + month)
+    new_fortune = Fortune(day=specdate, fortune=fortunemsg)
     db.session.add(new_fortune)
     db.session.commit()
-    last5 = Fortune2.query.order_by(desc("id")).limit(6).all()
-    return render_template("index.html", yearmon=specdate, luck=luck, fortune=fortunemsg, last5=last5)
+    last5 = Fortune.query.order_by(desc("id")).limit(6).all()
+    return render_template("index.html", day=specdate, fortune=fortunemsg, last5=last5)
