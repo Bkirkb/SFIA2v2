342dce07ed3d3b7c2cce9fd556daefcb38480879
Bkirkb <b.kirkby489@gmail.com>
2021-01-21 19:41:58 +0000

    tweaking

--- jenkins/test.sh
+++ jenkins/test.sh
@@ -2,9 +2,10 @@
 sudo apt-get update
 sudo apt install -y python3 python3-venv python3-pip
 
+cd ..
 python3 -m venv venv
 source venv/bin/activate
 pip3 install -r service1/requirements.txt
 pip3 install flask_testing pytest pytest-cov
-python3 -m pytest --cov --cov-report xml term-missing --junitxml junit.xml
+python3 -m pytest  --cov-report xml --cov-report term-missing --junitxml junit.xml
 deactivate
