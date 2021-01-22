776e7a75df928e76173a7c95182c8bc3ccf17a75
Brendan Kirkby <72553105+Bkirkb@users.noreply.github.com>
2021-01-22 17:14:01 +0000

    update to what is on main and the updated version

--- service1/application/routes.py
+++ service1/application/routes.py
@@ -9,12 +9,11 @@ from sqlalchemy import desc
 @app.route('/index')
 def index():
     #target is name of container and port to be published
-    year_response = requests.get("http://fortune-swarm_service2:5000/getyearmon")
+    day_response = requests.get("http://fortune-swarm_service2:5000/getday")
     luck_response = requests.get("http://fortune-swarm_service3:5000/getluck")
-    fortune_response_post = requests.post("http://fortune-swarm_service4:5000/getfortune", json={"year" : year_response.json()["year"] , "luck" : luck_response.text})
+    fortune_response_post = requests.post("http://fortune-swarm_service4:5000/getfortune", json={"day" : day_response.json()["day"] , "luck" : luck_response.text})
     
     fortunemsg = fortune_response_post.text
-
     day = str(day_response.json()["day"])
     month = str(day_response.json()["month"])
     specdate = str(day + " " + month)
