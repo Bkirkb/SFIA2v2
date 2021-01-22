5257bfe3351657cb3ad72f94d4269ea319b676dd
Brendan Kirkby <72553105+Bkirkb@users.noreply.github.com>
2021-01-22 14:59:12 +0000

    Pull from feature-2 into dev to update app version (#4)
    * feature2
    
    * final files
    
    * changed yaml file
    
    * 100% coverage
    
    * streamlined requirements
    
    * streamlined requirements
    
    * streamlined requirements
    
    * merge
    
    * merge

--- service3/app.py
+++ service3/app.py
@@ -5,14 +5,10 @@ app = Flask(__name__)
 
 @app.route("/getluck", methods=["GET"])
 def getluck():
-    #simulate a dice roll
-    luck = random.randint(1,6)
-    luck2 = random.randint(1,6)
-    if luck == luck2:
-        lucky = True
-    elif luck != luck2:
-        lucky = False
-    return  Response(str(lucky), mimetype='text/plain')
+    luck = random.randint(1,100)
+    return  Response(str(luck), mimetype='text/plain')
+
+
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
--- tests/test_service3.py
+++ tests/test_service3.py
@@ -2,8 +2,7 @@ from flask_testing import TestCase
 from unittest.mock import patch
 from flask import url_for
 from service3.app import app
-from random import randint
-
+from random import  randint
 
 class TestBase(TestCase):
     def create_app(self):
@@ -11,15 +10,9 @@ class TestBase(TestCase):
 
 class TestResponse(TestBase):
 
-    def testlucktrue(self):
-        with patch("random.randint") as random:
-            random.side_effect = [5,5]
-            response = self.client.get(url_for('getluck'))
-            self.assertEqual(b'True', response.data)
-
-    def testluckfalse(self):
-        with patch("random.randint") as random2:
-            random2.side_effect = [1,6]
-            response = self.client.get(url_for('getluck'))
-            self.assertEqual(b'False', response.data)
-    
+    def testgetluck(self):
+            with patch("random.randint") as random:
+                random.return_value = 3
+                response = self.client.get(url_for('getluck'))
+                self.assertIn(b'3', response.data)
+   
