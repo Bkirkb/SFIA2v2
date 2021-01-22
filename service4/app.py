0f5a6fe160af39d1ac21162e3aa1a07869684442
Bkirkb <b.kirkby489@gmail.com>
2021-01-22 15:30:31 +0000

    Fixed test1 service

--- service4/app.py
+++ service4/app.py
@@ -11,7 +11,7 @@ def getfortune():
 
     output = request.json
     day = output["day"]
-    luck = output["luck"]
+    luck = int(output["luck"])
 
     if int(day) in unlucky:
         luck -= 20
@@ -26,7 +26,7 @@ def getfortune():
     else:
         fortunemsg="You are the luckiest individual on earth, Your past, present and future has been blessed by higher powers the system predicts a lottery win in the near future"
 
-    return Response(str(fortunemsg), mimetype='text/plain')
+    return Response(str(fortunemsg) , mimetype='text/plain')
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
